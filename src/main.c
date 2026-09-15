cc0ed3918875f83e780c7166e02b6fc926ffa07c
Benjamin RAPPO <brappo@desktop-debian-home.home>
2026-09-15 15:43:32 +0200

    I now clean the screen

--- src/main.c
+++ src/main.c
@@ -5,6 +5,17 @@
 #include <SDL2/SDL_video.h>
 #include <strings.h>
 
+bool clear_screen(sdl_handler_t* sdl_handler) {
+	if (SDL_SetRenderDrawColor(sdl_handler->renderer, 0, 0, 0, 255) < 0
+		|| SDL_RenderClear(sdl_handler->renderer) < 0) {
+		fprintf(stderr, "%s", SDL_GetError());
+
+		return false;
+	}
+	SDL_RenderPresent(sdl_handler->renderer);
+	return true;
+}
+
 int main(void)
 {
 	sdl_handler_t	sdl_handler;
@@ -12,7 +23,8 @@ int main(void)
 
 	if (!initiated)
 		return EXIT_FAILURE;
-	SDL_Delay(3000);
+	clear_screen(&sdl_handler);
+	SDL_Delay(6000);
 	clear_handler(&sdl_handler);
     return EXIT_SUCCESS;
 }
